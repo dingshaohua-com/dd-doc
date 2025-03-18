8135eafb202d9246ddb9a913fd6d27d51a29178c
丁少华 <960423114@qq.com>
2025-03-18 23:07:11 +0800

    feat: go on

---
+++ src/web/src/components/menu-bar/cmp/font-style/index.tsx
@@ -0,0 +1,94 @@
+import {
+  RiBold,
+  RiItalic,
+  RiUnderline,
+  RiStrikethrough,
+} from '@remixicon/react';
+import './style.scss';
+import { Button, Tooltip } from 'antd';
+
+const buttonGroup: Array<any> = [
+  {
+    value: 'bold',
+    label: 'Bold',
+    icon: RiBold,
+    action: (editor) => editor.chain().focus().toggleBold().run(),
+    isActive: (editor) => editor.isActive('bold'),
+    canExecute: (editor) =>
+      editor.can().chain().focus().toggleBold().run() &&
+      !editor.isActive('codeBlock'),
+    tooltip: '粗体',
+  },
+  {
+    value: 'italic',
+    label: 'Italic',
+    icon: RiItalic,
+    action: (editor) => editor.chain().focus().toggleItalic().run(),
+    isActive: (editor) => editor.isActive('italic'),
+    canExecute: (editor) =>
+      editor.can().chain().focus().toggleItalic().run() &&
+      !editor.isActive('codeBlock'),
+    tooltip: '斜体',
+  },
+  {
+    value: 'underline',
+    label: 'Underline',
+    icon: RiUnderline,
+    action: (editor) => editor.chain().focus().toggleUnderline().run(),
+    isActive: (editor) => editor.isActive('underline'),
+    canExecute: (editor) =>
+      editor.can().chain().focus().toggleUnderline().run() &&
+      !editor.isActive('codeBlock'),
+    tooltip: '下划线',
+  },
+  {
+    value: 'strike',
+    label: 'Strike',
+    icon: RiStrikethrough,
+    action: (editor) => editor.chain().focus().toggleStrike().run(),
+    isActive: (editor) => editor.isActive('strike'),
+    canExecute: (editor) =>
+      editor.can().chain().focus().toggleUnderline().run() &&
+      !editor.isActive('codeBlock'),
+    tooltip: '删除线',
+  },
+  //   {
+  //     value: 'color',
+  //     label: 'Color',
+  //     icon: RiFontColor,
+  //     action: editor => editor.chain().focus().toggleStrike().run(),
+  //     isActive: editor => editor.isActive('strike'),
+  //     canExecute: editor => editor.can().chain().focus().toggleUnderline().run() && !editor.isActive('codeBlock'),
+  //     tooltip:"颜色",
+  //   },
+  //   {
+  //     value: 'dot',
+  //     label: 'Dot',
+  //     icon: <DotsHorizontalIcon className="size-5" />,
+  //     action: editor => editor.chain().focus().toggleDot().run(),
+  //     isActive: editor => editor.isActive('dot'),
+  //     canExecute: editor => editor.can().chain().focus().toggleUnderline().run() && !editor.isActive('codeBlock'),
+  //     tooltip:"强调",
+  //   },
+];
+
+const FontStyle = ({ editor }) => {
+  return (
+    <div className="fontStyle">
+      {buttonGroup.map(({ icon: Icon, tooltip, isActive, action, value }) => (
+        <Tooltip title={tooltip} key={value}>
+          <Button
+            onClick={() => action(editor)}
+            color="default"
+            variant={isActive(editor) ? 'solid' : 'filled'}
+            autoInsertSpace
+          >
+            <Icon />
+          </Button>
+        </Tooltip>
+      ))}
+    </div>
+  );
+};
+
+export default FontStyle;
---
+++ src/web/src/components/menu-bar/index.tsx
@@ -0,0 +1,26 @@
+import './style.scss';
+import Heading from './cmp/heading'
+import FontStyle from './cmp/font-style'
+import Shape from './cmp/shape';
+import Formula from './cmp/formula';
+import { Button, Divider } from "antd";
+
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div className="menuBar">
+      <Heading editor={editor}/>
+      {/* <Divider orientation="vertical" variant="middle" flexItem className='menuBarDivider' />
+      <FontStyle editor={editor}/>
+      <Divider orientation="vertical" variant="middle" flexItem className='menuBarDivider' />
+      <Shape editor={editor}/>
+      <Formula editor={editor}/> */}
+    </div>
+  );
+};
+
+export default MenuBar;
