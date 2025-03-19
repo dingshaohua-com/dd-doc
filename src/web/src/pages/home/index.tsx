89a6af96d1e1925b83326632af965fe9df0a485b
丁少华 <dingshaohua@douyuxingchen.com>
2025-03-19 18:46:10 +0800

    fix: 优化

--- src/web/src/pages/home/index.tsx
+++ src/web/src/pages/home/index.tsx
@@ -4,54 +4,23 @@ import MenuBar from "@/components/menu-bar";
 import MarkdownIt from "markdown-it";
 import { container } from "@mdit/plugin-container";
 import mdcont from "markdown-it-container";
+import hljs from "highlight.js";
 
+const highlight = (str, lang) => {
+  if (lang && hljs.getLanguage(lang)) {
+    try {
+      return hljs.highlight(lang, str).value;
+    } catch (__) {}
+  }
+  return ""; // 使用额外的默认转义
+};
 const md = MarkdownIt();
-md.use(mdcont, "tip", {
-  // validate(params) {
-  //   return params.trim().match(/^tip(\s+.*)?$/);
-  // },
-  // render(tokens, idx) {
-  //   const m = tokens[idx].info.trim().match(/^tip(\s+.*)?$/);
-  //   if (!m) {
-  //     console.error('正则匹配失败，tokens[idx].info:', tokens[idx].info);
-  //     return ''; // 返回空字符串，避免报错
-  //   }
-  //   let title = m[1].trim(); // 获取标题内容
-  //   // 如果没有标题，使用默认值
-  //   if (!title) {
-  //     title = "默认标题";
-  //   }
-
-  //   if (tokens[idx].nesting === 1) {
-  //     // 打开标签
-  //     return `<div class="tip"><p>${md.utils.escapeHtml(title)}</p>\n`;
-  //   } else {
-  //     // 关闭标签
-  //     return "</div>\n";
-  //   }
-  // },
-});
-md.use(mdcont, "danger", {
-  // validate: function (params) {
-  //   console.log("Params:", params); // 打印输入
-  //   const match = params.trim().match(/^danger\s+(.*)$/);
-  //   console.log("Match:", match); // 打印匹配结果
-  //   return match;
-  // },
-  // render: function (tokens, idx) {
-  //   const m = tokens[idx].info.trim().match(/^danger\s+(.*)$/);
-  //   if (tokens[idx].nesting === 1) {
-  //     //  opening tag
-  //     return '<div class="danger"><p>' + md.utils.escapeHtml(m[1]) + "</p>\n";
-  //   } else {
-  //     // closing tag
-  //     return "</div>\n";
-  //   }
-  // },
-});
+md.set({ highlight });
+md.use(mdcont, "tip");
+md.use(mdcont, "danger");
 function App() {
   const [content, setContent] = useState(`
-::: tip 提示
+::: tip
 你好我是 tip 类型的自定义容器
 :::
 
@@ -59,11 +28,6 @@ function App() {
 
 
 `);
-
-  // ::: danger 警告
-  // **提示**
-  // 你好我是 danger 类型的自定义容器
-  // :::
   const onContentIpt = (event: any) => {
     setContent(event.target.value);
   };
