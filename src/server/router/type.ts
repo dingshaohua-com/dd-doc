833b634c3079822ea6c36ac6fa9588eabd665831
丁少华 <dingshaohua@douyuxingchen.com>
2025-03-31 14:15:24 +0800

    fix: init

--- src/server/router/type.ts
+++ src/server/router/type.ts
@@ -4,27 +4,30 @@ import _ from "lodash";
 
 const prisma = new PrismaClient();
 
-
-
 const router = new Router({ prefix: "/type" });
 router.get("/", async (ctx) => {
-    let prismaParams: any = {};
-    const queryParams = _.cloneDeep((ctx.query || {}));
-    if (queryParams.includeBook) {
-        prismaParams['include'] = {
-            book: true
-        };
-        delete queryParams.includeBook;
-    }
-    Object.keys(queryParams).length > 0 && (prismaParams.select = queryParams);
-    const results = await prisma.type.findMany(prismaParams);
-    ctx.body = results;
+  let prismaParams: any = {};
+  const queryParams = _.cloneDeep(ctx.query || {});
+  if (queryParams.includeBook) {
+    prismaParams["include"] = {
+      book: true,
+    };
+    delete queryParams.includeBook;
+  }
+  const haveParams = Object.keys(queryParams).length > 0;
+  let results;
+  if (haveParams) {
+    queryParams.id && (queryParams.id = Number(queryParams.id)); 
+    prismaParams.where = queryParams;
+    results = await prisma.type.findFirst(prismaParams);
+  } else {
+    results = await prisma.type.findMany(prismaParams);
+  }
+  ctx.body = results;
 });
 
-
-
-router.post('/', async (ctx) => {
-    const results = await prisma.type.create({ data: ctx.request.body });
-    ctx.body = results;
-})
+router.post("/", async (ctx) => {
+  const results = await prisma.type.create({ data: ctx.request.body });
+  ctx.body = results;
+});
 export default router;
--- src/web/src/pages/detail/index.tsx
+++ src/web/src/pages/detail/index.tsx
@@ -1,57 +1,79 @@
-
 import "./style.scss";
 import { useEffect, useState } from "react";
-import { Button } from "antd"
+import { Button, Divider } from "antd";
 import Editor from "@/components/editor";
-import noDocImg from '@/assets/no-doc.svg'
-
+import noDocImg from "@/assets/no-doc.svg";
+import { PlusCircleOutlined } from "@ant-design/icons";
+import { useSearchParams } from "react-router";
+import libImg from "@/assets/lib.svg"
 
 function Detail() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+
+
+  // const [showEditor, setShowEditor] = useState(false);
   const [content, setContent] = useState("");
+
+  const [type, setType] = useState({name:''});
+  const syncType = async () => {
+    const res = await api.type.get({ id });
+    setType(res);
+  };
 
   const [docs, setDocs] = useState([]);
   const syncDocs = async () => {
     const res = await api.doc.get();
-    setDocs(res)
-  }
+    setDocs(res);
+  };
 
   const toSave = async () => {
     console.log(111, content);
     const res = await api.doc.add();
+  };
+
+  const createDoc = async()=>{
+    const res = await api.doc.add({name:'无标题文档'});
+    syncDocs();
   }
 
   useEffect(() => {
+    syncType();
     syncDocs();
-  }, [])
+  }, []);
   return (
     <div className="detail-cmp">
       <div className="navbar">
+        <div></div>
         <div>
-
+          <Button type="primary" onClick={toSave}>
+            保存
+          </Button>
         </div>
-        <div>
-          <Button type="primary" onClick={toSave}>保存</Button>
-        </div>
-
       </div>
       <div className="content">
         <div className="docs">
-
+          <div className="title"> <img src={libImg} /> {type.name}</div>
+          <Divider />
+          <div></div>
         </div>
         <div className="editor">
-          {docs.length > 0 ? <div className="data">
-            <Editor content={content} setContent={setContent} />
-          </div> : <div className="no-data">
-            <div>
-            <img src={noDocImg} />
-            <div>创建第一篇</div>
+          {(docs.length > 0 )? (
+            <div className="data">
+              <Editor content={content} setContent={setContent} />
             </div>
-           
-          </div>}
-
+          ) : (
+            <div className="no-data">
+              <div>
+                <img src={noDocImg} />
+                <div onClick={createDoc}>
+                  <PlusCircleOutlined /> 创建第一篇
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
-
     </div>
   );
 }
